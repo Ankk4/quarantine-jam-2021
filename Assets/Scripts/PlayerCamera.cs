e9cc7286a881f98c325a5d0d1f623201511d3a20
ankk4 <antero.juutinen@gmail.com>
2021-03-19 18:36:47 +0200

    Init

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float yOffset, zOffset;
+    [SerializeField]
+    private GameObject[] targets;
+
+    private int currentTarget;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = new Vector3(0, yOffset, zOffset);
+    }
+
+    void LateUpdate ()
+    {
+        transform.position = targets[currentTarget].transform.position + offset;
+    }
+
+    public void SetPlayerTarget()
+    {
+        currentTarget = 0;
+    }
+
+    public void SetGhostTarget()
+    {
+        currentTarget = 1;
+    }
+}
+
